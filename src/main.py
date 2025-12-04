c791c436d2fa22b915b374768aa25034bbb74626
Linus Lam <linuslyt@gmail.com>
2025-12-04 10:52:46 -0800

    Image upload endpoint; placeholder endpoints for processing

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+import asyncio
+from fastapi import FastAPI, File, HTTPException, UploadFile, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
-import asyncio
+import io
+import json
+import os
+from pydantic import BaseModel
+from PIL import Image, UnidentifiedImageError
 import torch
+from typing import Union
 
 from helpers.utils import (get_most_free_gpu)
 
@@ -24,6 +30,72 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+UPLOAD_DIR = "./uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+@app.post("/upload/", status_code=status.HTTP_201_CREATED)
+async def save_image(file: UploadFile = File(...)):
+    print("Processing uploaded image...")
+    if file.content_type not in ["image/jpeg", "image/png", "image/webp"]:
+        raise HTTPException(status_code=400, detail="Invalid file type. Only JPEG, PNG, and WebP are accepted.")
+
+    try:
+        contents = await file.read()
+        image = Image.open(io.BytesIO(contents))
+
+        # LLaVA processor (based on CLIP) expects 3 channels.
+        if image.mode != "RGB":
+            image = image.convert("RGB")
+
+        # Save as JPG. Resizing etc. will be handled by LLaVA processor
+        new_filename = f"{file.filename.split('.')[0]}.jpg"
+        save_path = os.path.join(UPLOAD_DIR, new_filename)
+        image.save(save_path, "JPEG", exist_ok=True)
+
+        return {
+            "filename": new_filename,
+        }
+
+    except UnidentifiedImageError:
+        raise HTTPException(status_code=400, detail="Uploaded file is not a valid image.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# TODO: typing
+def new_event(event_type: str, data: Union[str, dict]):
+    # https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#event_stream_format
+    return f"event: {event_type}\ndata: {data if type(data) == str else json.dumps(data)}\n\n"
+
+async def captioning_pipeline():
+    await asyncio.sleep(0.5)
+    yield new_event(event_type="log", data="Generating image caption...")
+
+    await asyncio.sleep(0.5)
+    caption = "placeholder"
+    yield new_event(event_type="scores", data=caption) # On frontend, retrieve with event.data
+
+@app.get("/caption-image")
+async def caption_image():
+    return StreamingResponse(captioning_pipeline(), media_type="text/event-stream")
+
+async def importance_estimation_piipeline():
+    await asyncio.sleep(0.5)
+    yield new_event(event_type="log", data="Performing dictionary learning...")
+
+    await asyncio.sleep(0.5)
+    yield new_event(event_type="log", data="Calculating concept importance...")
+
+    # Scores
+    await asyncio.sleep(0.5)
+    scores = {
+        "importance": [],
+        "activations": [],
+    }
+    yield new_event(event_type="scores", data=scores) # On frontend, retrieve with JSON.parse(event.data);
+
+@app.get("/importance-estimation")
+async def importance_estimation():
+    return StreamingResponse(importance_estimation_piipeline(), media_type="text/event-stream")
 
 @app.get("/status")
 def get_status(): # Return server and CUDA status
